701f6cc23c17116e694c2f15d8dd7142d51c12c7
Seungtae Nam <stnamjef@naver.com>
2021-07-07 22:32:07 +0900

    2021-07-07

--- source/chapter3.py
+++ source/chapter3.py
@@ -25,6 +25,35 @@ def q2():
     print(result)
 
 
+def q3():
+    N, M = map(int, input().split())
+    
+    max_val = 0
+    for i in range(N):
+        row = list(map(int, input().split()))
+        min_row = min(row)
+        if max_val < min_row:
+            max_val = min_row
+    
+    print(max_val)
+
+
+def q4():
+    N, K = map(int, input().split())
+    
+    count = 0
+    while N % K != 0:
+        N -= 1
+        count += 1
+    
+    while N // K != 0:
+        N //= K
+        count += 1
+    
+    print(count)
+
 if __name__ == '__main__':
-    q1()
-    q2()
+    # q1()
+    # q2()
+    # q3()
+    q4()
